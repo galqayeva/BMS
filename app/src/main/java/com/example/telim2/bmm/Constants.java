04fb3828abd1f6442450bbbac31f8e2159601b0f
galqayeva <galqayeva@gmail.com>
2017-09-19 17:17:26 +0400

    drawer

--- app/src/main/java/com/example/telim2/bmm/Constants.java
+++ app/src/main/java/com/example/telim2/bmm/Constants.java
@@ -8,9 +8,15 @@ public class Constants {
 
     public  static  String LOGIN_URL="http://82.194.6.3:1213/API/";
 
+    //////drawer menu
+
+    public  static  String DRAWER_MENU_GUNLUK="Gundelik";
+    public static  String DRAWER_MENU_CHAT="Mesajlasma";
+
 
     ///Strings
 
     public  static String PLEASE_FILL_USERNAME_AND_PASS="username ve passwordu daxil edin";
     public  static String WRONG_PASSWORD_OR_USERNAME="yalnis username ve ya password";
+
 }
--- app/src/main/java/com/example/telim2/bmm/MainActivity.java
+++ app/src/main/java/com/example/telim2/bmm/MainActivity.java
@@ -4,16 +4,54 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+
+
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("home");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(1).withName("gnlk");
+
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,item2,
+                        new DividerDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        switch (position){
+                            case 1:Toast.makeText(MainActivity.this, "aa", Toast.LENGTH_LONG).show();break;
+                            case 2:Toast.makeText(MainActivity.this, "aa", Toast.LENGTH_LONG).show();break;
+                        }
+
+                        return true;
+                    }
+                })
+                .build();
+
 
 
     }
