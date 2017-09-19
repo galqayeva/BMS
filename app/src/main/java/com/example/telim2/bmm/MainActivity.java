37e95336e758fbce3eb38a9a758594ec9e64207a
galqayeva <galqayeva@gmail.com>
2017-09-19 18:28:32 +0400

    drawer

--- app/src/main/java/com/example/telim2/bmm/MainActivity.java
+++ app/src/main/java/com/example/telim2/bmm/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.telim2.bmm;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.AccountHeader;
@@ -18,10 +22,15 @@ import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    ImageView imageView;
+    SharedPreferences sharedpreferences;
+    public static final String mypreference = "mypref";
+    TextView tw1,tw2,tw3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar=(Toolbar)findViewById(R.id.toolbar);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        tw1=(TextView)findViewById(R.id.textView4);
+        tw2=(TextView)findViewById(R.id.textView5);
+        tw3=(TextView)findViewById(R.id.textView6);
+
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+
+        tw1.setText(sharedpreferences.getString("name", ""));
+        tw2.setText(sharedpreferences.getString("surname", ""));
+        tw3.setText(sharedpreferences.getString("father", ""));
+
+        Picasso.with(getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").transform(new CircleTransform()).into(imageView);
 
         drawMenu(toolbar);
 
