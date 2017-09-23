972997bdb99919445a503f3f488f5437a960cc9a
galqayeva <galqayeva@gmail.com>
2017-09-23 14:55:11 +0400

    frgmnt one

--- app/src/main/java/com/example/telim2/bmm/Fragments/FragmentOne.java
+++ app/src/main/java/com/example/telim2/bmm/Fragments/FragmentOne.java
@@ -47,12 +47,12 @@ public class FragmentOne extends Fragment {
 
     Spinner spinner;
     Button ok,monday,tuesday,wednesday,thursday,friday;
-    RecyclerView recyclerView,recyclerView2;
-    int k=1,a=1,dm=1;
+    RecyclerView rV1,rV2,rV3,rV4,rV5;
+    int k=1,a=1,dm=0;
     public static final String mypreference = "mypref";
     SharedPreferences sharedpreferences;
-    private List<LessonTableModel> modelList,modelList2;
-    private RecyclerView.Adapter adapter2;
+    private List<LessonTableModel> modelList1,modelList2,modelList3,modelList4,modelList5;
+    private RecyclerView.Adapter adapter1,adapter2,adapter3,adapter4,adapter5;
 
 
     @Nullable
@@ -69,72 +69,33 @@ public class FragmentOne extends Fragment {
         friday=(Button)view.findViewById(R.id.buttonFriday);
         ok=(Button)view.findViewById(R.id.buttonOk);
 
-        recyclerView=(RecyclerView)view.findViewById(R.id.recycleview);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView2=(RecyclerView)view.findViewById(R.id.recycleview2);
-        recyclerView2.setHasFixedSize(true);
-        recyclerView2.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rV1=(RecyclerView)view.findViewById(R.id.recycleview);
+        rV1.setHasFixedSize(true);
+        rV1.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rV2=(RecyclerView)view.findViewById(R.id.recycleview2);
+        rV2.setHasFixedSize(true);
+        rV2.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rV3=(RecyclerView)view.findViewById(R.id.recycleview3);
+        rV3.setHasFixedSize(true);
+        rV3.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rV4=(RecyclerView)view.findViewById(R.id.recycleview4);
+        rV4.setHasFixedSize(true);
+        rV4.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rV5=(RecyclerView)view.findViewById(R.id.recycleview5);
+        rV5.setHasFixedSize(true);
+        rV5.setLayoutManager(new LinearLayoutManager(getActivity()));
 
 
-        modelList=new ArrayList<>();
+
+
+        modelList1=new ArrayList<>();
         modelList2=new ArrayList<>();
+        modelList3=new ArrayList<>();
+        modelList4=new ArrayList<>();
+        modelList5=new ArrayList<>();
         sharedpreferences = this.getActivity().getSharedPreferences(mypreference, Context.MODE_PRIVATE);
 
-        getGrades();
 
-
-
-        monday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(k==1){
-                    tuesday.setVisibility(View.GONE);
-                    wednesday.setVisibility(View.GONE);
-                    thursday.setVisibility(View.GONE);
-                    friday.setVisibility(View.GONE);
-                    recyclerView.setVisibility(View.VISIBLE);
-                    k++;
-                }
-                else{
-                    tuesday.setVisibility(View.VISIBLE);
-                    wednesday.setVisibility(View.VISIBLE);
-                    thursday.setVisibility(View.VISIBLE);
-                    friday.setVisibility(View.VISIBLE);
-                    recyclerView.setVisibility(View.GONE);
-                    k--;
-                }
-            }
-        });
-        tuesday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(a==1){
-                    monday.setVisibility(View.GONE);
-                    wednesday.setVisibility(View.GONE);
-                    thursday.setVisibility(View.GONE);
-                    friday.setVisibility(View.GONE);
-                    recyclerView.setVisibility(View.GONE);
-                    recyclerView2.setVisibility(View.VISIBLE);
-                    a++;
-                }
-                else{
-                    monday.setVisibility(View.VISIBLE);
-                    wednesday.setVisibility(View.VISIBLE);
-                    thursday.setVisibility(View.VISIBLE);
-                    friday.setVisibility(View.VISIBLE);
-                    recyclerView2.setVisibility(View.GONE);
-                    a--;
-                }
-            }
-        });
-
-
-        return view;
-
-    }
-
-    public void getGrades(){
         StringRequest stringRequest=new StringRequest(Request.Method.POST, Constants.LOGIN_URL,
                 new Response.Listener<String>() {
                     @Override
@@ -150,33 +111,55 @@ public class FragmentOne extends Fragment {
                                 for (int i=0;i<2;i++){
                                     JSONObject jsonObjectdemo=jsonArray.getJSONObject(i);
 
-                                    for (int j=0;j<jsonObjectdemo.getJSONArray("subjects").length();j++){
+                                    int length=jsonObjectdemo.getJSONArray("subjects").length();
+                                    for (int j=0;j<length;j++){
 
                                         JSONObject jsonObjectLesson=jsonObjectdemo.getJSONArray("subjects").getJSONObject(j);
                                         String name=jsonObjectLesson.getString("name");
                                         String grade=jsonObjectLesson.getString("grade");
 
-                                        if(dm<=11){
+                                        if(dm<length){
                                             LessonTableModel demo=new LessonTableModel(name,grade);
-                                            modelList.add(demo);
+                                            modelList1.add(demo);
                                             dm++;
                                         }
-                                        else if (dm>11){
+                                        else if (dm>=length && dm<2*length){
                                             LessonTableModel demo2=new LessonTableModel(name,grade);
                                             modelList2.add(demo2);
                                             dm++;
                                         }
-
+                                        else if (dm>=2*length && dm<3*length){
+                                            LessonTableModel demo3=new LessonTableModel(name,grade);
+                                            modelList3.add(demo3);
+                                            dm++;
+                                        }
+                                        else if (dm>=3*length && dm<4*length){
+                                            LessonTableModel demo4=new LessonTableModel(name,grade);
+                                            modelList4.add(demo4);
+                                            dm++;
+                                        }
+                                        else if (dm>=4*length && dm<5*length){
+                                            LessonTableModel demo5=new LessonTableModel(name,grade);
+                                            modelList5.add(demo5);
+                                            dm++;
+                                        }
 
                                     }
 
-                                    RecyclerView.Adapter adapter=new MyAdapter(modelList,getActivity());
-                                    recyclerView.setAdapter(adapter);
+                                    adapter1=new MyAdapter(modelList1,getActivity());
+                                    rV1.setAdapter(adapter1);
 
+                                    adapter2=new MyAdapter(modelList2,getActivity());
+                                    rV2.setAdapter(adapter2);
 
-                                    RecyclerView.Adapter adapter2=new MyAdapter(modelList2,getActivity());
-                                    recyclerView2.setAdapter(adapter2);
+                                    adapter3=new MyAdapter(modelList3,getActivity());
+                                    rV3.setAdapter(adapter3);
 
+                                    adapter4=new MyAdapter(modelList4,getActivity());
+                                    rV4.setAdapter(adapter4);
+
+                                    adapter5=new MyAdapter(modelList5,getActivity());
+                                    rV5.setAdapter(adapter5);
 
 
                                 }
@@ -208,5 +191,57 @@ public class FragmentOne extends Fragment {
             }
         };
         MySingleTon.getInstance(getActivity()).addToRequestQueue(stringRequest);
+
+
+
+        monday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(k==1){
+                    tuesday.setVisibility(View.GONE);
+                    wednesday.setVisibility(View.GONE);
+                    thursday.setVisibility(View.GONE);
+                    friday.setVisibility(View.GONE);
+                    rV1.setVisibility(View.VISIBLE);
+                    k++;
+                }
+                else{
+                    tuesday.setVisibility(View.VISIBLE);
+                    wednesday.setVisibility(View.VISIBLE);
+                    thursday.setVisibility(View.VISIBLE);
+                    friday.setVisibility(View.VISIBLE);
+                    rV1.setVisibility(View.GONE);
+                    k--;
+                }
+            }
+        });
+        tuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(a==1){
+                    monday.setVisibility(View.GONE);
+                    wednesday.setVisibility(View.GONE);
+                    thursday.setVisibility(View.GONE);
+                    friday.setVisibility(View.GONE);
+                    rV1.setVisibility(View.GONE);
+                    rV2.setVisibility(View.VISIBLE);
+                    a++;
+                }
+                else{
+                    monday.setVisibility(View.VISIBLE);
+                    wednesday.setVisibility(View.VISIBLE);
+                    thursday.setVisibility(View.VISIBLE);
+                    friday.setVisibility(View.VISIBLE);
+                    rV2.setVisibility(View.GONE);
+                    a--;
+                }
+            }
+        });
+
+
+        return view;
+
     }
+
+
 }
